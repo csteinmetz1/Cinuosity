e3391e4d82f3c85fa6f21dcd6acf4f904c354366
csteinmetz1 <csteinmetz1@gmail.com>
2017-12-29 18:14:54 -0500

    added python script to compute usage statistics

---
+++ calc_stats.py
@@ -0,0 +1,27 @@
+import sys
+import json
+from pprint import pprint
+
+def calculate(filename):
+
+    data = json.load(open('stats.json'))
+
+    total_weirdness = 0
+
+    for playlist in data['statistics']:
+        weirdness = float(playlist['weirdess'])
+        total_weirdness += weirdness
+
+    total_playlists = len(data['statistics'])
+    average_weirdness = total_weirdness / total_playlists
+
+    print("Average weirdness:",  average_weirdness)
+    print("Playlists generated:", total_playlists)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        calculate(filename)
+    else:
+        print("Usage: python calc_stats.py path/to/stats.json")
